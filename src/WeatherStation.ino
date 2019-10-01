854141d4cee172b58d7c20b17edd5e30eb355257
Toby MIlls <Mills.toby@gmail.com>
2019-10-01 18:58:51 +0200

    Keep track of last 10 Averaged light readings

--- src/WeatherStation.ino
+++ src/WeatherStation.ino
@@ -1,19 +1,28 @@
 int reading [10];
 int nextReadingIndex = 0;
-int averageReading = 0;
 int lastReading = 0;
 
+int averageReading [10];
+int lastAverageReadingIndex = 0;
+int lastAverageReading = 0;
+
 int timeOfLastReading = 0;
-int readingSeparation = 60000; // 1 minute delay between readings
 int timeOfLastPublish = 0;
+
+//Constants
+char *sourceCode = "https://github.com/Toby-Mills/WeatherStation";
+int readingSeparation = 60000; // 1 minute delay between readings
 int publishSeparation = 600000; // 10 minute delay between publishing average reading
 
-
 void setup() {
-
+  //pins
   pinMode(A0,INPUT_PULLUP);
+  
+  //Register variables and methods to allow control via Particle Cloud
+  Particle.variable("sourceCode", sourceCode, STRING);
   Particle.variable("lastReading", lastReading);
   Particle.variable("averageReading", averageReading);
+
 
   timeOfLastReading = -readingSeparation;
 }
@@ -26,16 +35,20 @@ void loop() {
   }
 
   if(millis() > timeOfLastPublish + publishSeparation){
-    publishAverageReading(averageReading);
+    publishAverageReading();
   }
 
 }
 
 //--------------------------------------------------------------------------------------
-void publishAverageReading(int newValue){
-  char publishString[40];
-  sprintf(publishString, "%d", newValue);
-  Particle.publish("lightReading",publishString );
+void publishAverageReading(){
+  int valueToPublish;
+  char stringToPublish[40];
+
+  valueToPublish = averageReading[nextAverageReadingIndex -1];
+  sprintf(stringToPublish, "%d", valueToPublish);
+  Particle.publish("lightReading",stringToPublish );
+
   timeOfLastPublish = millis();
 }
 
@@ -59,6 +72,11 @@ void addNewReading(){
 void updateAverageReading(){
   int sumOfReadings = 0;
   int countOfReadings = 0;
+  int previousAverageReading = 0;
+  int previousAverageReadingIndex = 0;
+
+  int previousAverageReadingIndex = lastAverageReadingIndex;
+  int previousAverageReading = averageReading[previousAverageReadingIndex];
 
   for ( int readingIndex = 0; readingIndex < 10; readingIndex++ ) {
     if (reading[ readingIndex ] > 0){
@@ -66,5 +84,10 @@ void updateAverageReading(){
       countOfReadings++;
     }
   }
-  averageReading = sumOfReadings / countOfReadings;
+  
+  lastAverageReadingIndex++;
+  lastAverageReadingIndex = lastAverageReadingIndex % 10;
+  averageReading[lastAverageReadingIndex] = sumOfReadings / countOfReadings;
+
+  
 }
